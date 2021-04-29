2c4b998d219604600fa854669a0c1bfdd9bfa665
Mohammad Adiyudha <18102238@ittelkom-pwt.ac.id>
2021-04-29 12:29:51 +0700

    Penyesuaian Dengan  High Fidelity Design

--- program/index.php
+++ program/index.php
@@ -3,13 +3,13 @@
 ?>
 <html>
 <head>
-    <title>Document</title>
+    <title>E-Voting</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/custom_style.css">
 </head>
 <body>
     <!-- Navbar -->
-    <nav class="py-3 navbar navbar-expand-lg fixed-top">
+    <nav class="py-2 navbar navbar-expand-lg fixed-top">
         <div class="container">
             <a class="navbar-brand" href="#">
                 E-Voting
@@ -42,9 +42,8 @@
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <h1>Tes <br>ini coba h1</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti, saepe.</p>
-                    <button type="button" class="btn btn-dark">Voting</button>
+                    <h1>Berikan Suaramu Sekarang Demi Kemajuan Bangsa dan Negara</h1>
+                    <button type="button" class="btn btn-light"><strong>Vote Sekarang</strong></button>
                 </div>
                 <div class="col-5 img-col" id="ilustrasi">
                     <img src="images/voting-logo.png" alt="Hero Image" class="float-end">
@@ -65,28 +64,31 @@
         <div class="row row-cols-1 row-cols-md-3 g-4">
             <div class="col">
                 <div class="card">
-                <img src="images/thanos.jpg" class="card-img-top" alt="...">
+                <img src="images/thanosnos.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title">Suthanos</h5>
+                    <p class="card-text">Calon yang berasal dari RT 03 RW 09 ini memiliki hobi menanam terong.
+                    Sehingga warna kulitnya sendiri dicat sewarna dengan terong.</p>
                 </div>
                 </div>
             </div>
             <div class="col">
                 <div class="card">
-                <img src="images/ironman.jpg" class="card-img-top" alt="...">
+                <img src="images/ironman_tony.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title">Tony Sutorak</h5>
+                    <p class="card-text">Selain memiliki predikat S3 Teknik Mesin Robot,
+                    dia juga seringkali membantu masyarakat melakukan service elektronik secara gratis dan juga sering mengadakan giveaway obeng.</p>
                 </div>
                 </div>
             </div>
             <div class="col">
                 <div class="card">
-                <img src="images/thanos.jpg" class="card-img-top" alt="...">
+                <img src="images/hulk.jpg" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                    <h5 class="card-title">Hulak</h5>
+                    <p class="card-text">Seorang yang memiliki rumah di pinggiran desa.
+                    Hal ini dikarenakan sawah yang dimilikinya sangat luas. Dia adalah seorang Vegan sehingga warna kulitnya menjadi hijau</p>
                 </div>
                 </div>
             </div>
